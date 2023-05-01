66f930f11cab84cf3bb87b910be6696757c74be3
Brendan Poole <brendanpoole2002@gmail.com>
2023-05-01 17:36:54 -0400

    feat: Refuse non-numeric inputs
    Throw an exception for non-numeric inputs, testAlphabeticInput() complete.

--- src/main/java/ResolutionParser.java
+++ src/main/java/ResolutionParser.java
@@ -5,7 +5,12 @@ public class ResolutionParser {
         int[] resolutionArray = new int[2];
         String[] tempResolutionArray = stringResolution.split("x");
 
-        resolutionArray = Arrays.stream(tempResolutionArray).mapToInt(Integer::parseInt).toArray();
+        try {
+            resolutionArray = Arrays.stream(tempResolutionArray).mapToInt(Integer::parseInt).toArray();
+        } catch (NumberFormatException invalidInputException){
+            System.out.println("The resolution values may only use numeric characters! (e.g. \"256x256\")");
+            throw invalidInputException;
+        }
 
         return resolutionArray;
     }
