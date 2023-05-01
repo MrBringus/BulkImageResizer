03388f4a1389c2c854498a5f08f3a5023f834dd7
Brendan Poole <brendanpoole2002@gmail.com>
2023-05-01 01:06:24 -0400

    feat: Implement resolution parsing
    Implement basic resolution input parsing in ResolutionParser.parse(), testValidInput() complete.

--- src/main/java/ResolutionParser.java
+++ src/main/java/ResolutionParser.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class ResolutionParser {
     public int[] parse(String stringResolution) {
         int[] resolutionArray = new int[2];
+        String[] tempResolutionArray = stringResolution.split("x");
+
+        resolutionArray = Arrays.stream(tempResolutionArray).mapToInt(Integer::parseInt).toArray();
+
         return resolutionArray;
     }
 }
