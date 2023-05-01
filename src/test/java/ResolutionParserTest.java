08f6b6186058b35fb227eeda81e00b1a823995a5
Brendan Poole <brendanpoole2002@gmail.com>
2023-05-01 00:45:16 -0400

    test: Create ResolutionParserTest and a test for valid inputs
    Add a test class for ResolutionParser and create a test for valid inputs in its parsing method.

---
+++ src/test/java/ResolutionParserTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResolutionParserTest {
+    public ResolutionParser resolutionParser;
+    public String stringResolution;
+    public int[] resolutionArray;
+    public int[] testArray;
+
+    @BeforeEach
+    public void beforeEach() {
+        resolutionParser = new ResolutionParser();
+        stringResolution = "";
+        resolutionArray = new int[2];
+        testArray = new int[2];
+    }
+
+    @Test
+    public void testValidInput() {
+        stringResolution = "256x256";
+        resolutionArray = resolutionParser.parse(stringResolution);
+        testArray = new int[]{256, 256};
+        assertArrayEquals(testArray, resolutionArray);
+    }
+}
