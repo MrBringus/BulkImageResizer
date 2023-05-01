ec873862e649d81226ca88047ec397d3362c7b55
Brendan Poole <brendanpoole2002@gmail.com>
2023-05-01 17:50:36 -0400

    refactor: Place main resolution dialog into its own method
    Create obtainAndParseResolution() and move the dialog for obtaining the resolution into its own method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static final String VERSION = "2023-05-01";
     public static ResolutionParser resolutionParser = new ResolutionParser();
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hello, welcome to Bulk Image Resizer version " + VERSION + "!");
         System.out.println("Please place the images that you would like to resize in the \"input\" folder if you haven't already! Subfolders will also be read.\n");
-        Scanner input = new Scanner(System.in);
+
+        obtainAndParseResolution();
+
+        System.out.println("Which resampling algorithm would you prefer to use?");
+        String algorithm = input.nextLine();
+    }
+
+    public static int[] obtainAndParseResolution() {
         System.out.println("What resolution would you like to resize the images to? (e.g. \"256x256\")");
         String stringResolution = input.nextLine();
         int[] resolutionArray = resolutionParser.parse(stringResolution);
-        System.out.println("Which resampling algorithm would you prefer to use?");
-        String algorithm = input.nextLine();
+
+        return resolutionArray;
     }
 }
