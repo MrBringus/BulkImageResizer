53c18fe4b5b6acc4d51a7b62badcbfaffac2d0b8
Brendan Poole <brendanpoole2002@gmail.com>
2023-08-05 17:00:03 -0400

    test: Initialize ImageResizerTest
    Set up ImageResizerTest for testing functionality.

--- src/test/java/ImageResizerTest.java
+++ src/test/java/ImageResizerTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ImageResizerTest {
+    private ImageResizer imageResizer;
+
+    @BeforeEach
+    public void beforeEach() {
+        imageResizer = new ImageResizer();
+    }
 }
